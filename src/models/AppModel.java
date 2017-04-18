9fff146d0d864dadd64bb10cfcfcaf499b70eb39
rd222dv <ruthinkaa@hotmail.com>
2017-04-18 18:56:35 +0200

    Add files via upload

---
+++ src/models/AppModel.java
@@ -0,0 +1,29 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AppModel {
+	private List<TimelineModel> tml;
+	AppController c; // ???
+
+	public AppModel() {
+		tml = new ArrayList<TimelineModel>();
+	}
+
+	public void setApp(List<TimelineModel> AppModel) {
+		this.tml = AppModel;
+	}
+
+	public List<TimelineModel> getApp() {
+		return tml;
+	}
+
+	public void add(TimelineModel tm) {
+		tml.add(tm);
+	}
+
+	public void remove(TimelineModel tm) {
+		tml.remove(tm);
+	}
+}
