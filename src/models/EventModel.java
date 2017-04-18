58059a4fb006832134e1d9a261090b136301d446
madouidris <madouidris@gmail.com>
2017-04-18 22:41:25 +0200

    Add files via upload

---
+++ src/models/EventModel.java
@@ -0,0 +1,72 @@
+package JavaFXDatabase;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Month;
+import java.util.Date;
+
+
+
+public class EventModel {
+	
+	private   Date  startDate;
+	private  Date endDate;
+	private  String  description;
+	private String name;
+
+	
+	public EventModel(Date startDate, Date endDate, String name, String description) throws ParseException{
+		
+          
+          Date StartDate = new Date(07/8/2017);
+          Date EndDate = new Date(04/05/2017);
+          
+          if(startDate.after(EndDate)){
+        	  throw new IllegalArgumentException();
+          }else if(startDate.after(EndDate)){
+        	  System.out.println("correct");
+          }
+        	  
+		this.name = name;
+		  this.description = description;
+		  this.startDate = startDate;
+		  this.endDate =  endDate;
+		  
+		  
+	}
+	
+	public String getName(){
+		return name;
+	}
+	public String getDescription(){
+		return description;
+	}
+	public Date getStartDate(){
+		return startDate;
+	}
+	public Date getEndDate(){
+		return endDate;
+	}
+	public void setName(String n){
+		this.name = n;
+	}
+	public void setDescription(String d){
+	    this.description= d;
+	}
+	public void setStartDate(Date sd){
+		this.startDate = sd;
+	}
+	public void setEndDate(Date ed){
+		this.endDate  = ed;
+	}
+
+     
+	
+	
+	
+		
+		
+		
+	}
+	
+
