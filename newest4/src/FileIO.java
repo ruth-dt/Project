d85bf9d2ca1cf4dd3361d8ba3544f9ede960c305
Robin Hagg <rh222di@student.lnu.se>
2017-05-25 14:21:52 +0200

    FileIO errors and comments

--- newest4/src/FileIO.java
+++ newest4/src/FileIO.java
@@ -5,34 +5,60 @@ import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 
-
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.stage.FileChooser;
 
 public class FileIO {
 	private File lastFile;
 	
+	/**
+	 * Method is called with current AppModel when a user clicks "save" in the menu
+	 * If lastFile is null (no file has been opened or saved to), then saveAs is called instead
+	 * @param am
+	 */
 	public void save(AppModel am){
 		if(lastFile != null)
 			jaxbObjectToXML(am, lastFile);
 		else
 			saveAs(am);
 	}
+	/**
+	 * Method is called with current AppModel when a user clicks "Save as" in the menu
+	 * A File Chooser is opened allowing the user to select where to save the .tlp (Timeline Project) file
+	 * @param am
+	 */
 	public void saveAs(AppModel am) {
 		FileChooser chooser = new FileChooser();
 
-		FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("XML files (*.tlp)", "*.tlp");
+		FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("Timeline Project Filest (*.tlp)", "*.tlp");
 		chooser.getExtensionFilters().add(extFilter);
 
 		File file = chooser.showSaveDialog(null);
+
+		if(file == null)
+			return; //Cancelled
+		
 		lastFile = file;
 		jaxbObjectToXML(am, file);
 	}
 
+	/**
+	 * Method is called when a user clicks "Open" in the menu (via AppController)
+	 * A file chooser is shown, allowing the user to open a .tlp file
+	 * @return
+	 */
 	public AppModel load() {
 		FileChooser chooser = new FileChooser();
-		File file = chooser.showOpenDialog(null);
-		FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("XML files (*.tlp)", "*.tlp");
+		
+		FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("Timeline Project Files (*.tlp)", "*.tlp");
 		chooser.getExtensionFilters().add(extFilter);
+		
+		File file = chooser.showOpenDialog(null);
+		
+		if(file == null)
+			return null; //Cancelled
+		
 		lastFile = file;
 		return jaxbXMLToObject(file);
 	}
@@ -44,8 +70,11 @@ public class FileIO {
 			AppModel emp = (AppModel) un.unmarshal(loadthisfile);
 			return emp;
 		} catch (Exception e) {
-			//e.printStackTrace();
-			//User cancelled
+			Alert alert = new Alert(AlertType.WARNING);
+			alert.setTitle("Error");
+			alert.setHeaderText("I/O Error");
+			alert.setContentText("An error was encountered while loading the file: " + e.getMessage());
+			alert.showAndWait();
 		}
 		return null; //Fixme
 	}
@@ -58,8 +87,11 @@ public class FileIO {
 			m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
 			m.marshal(emp, loadthisfile);
 		} catch (Exception e) {
-			//e.printStackTrace();
-			//User cancelled
+			Alert alert = new Alert(AlertType.WARNING);
+			alert.setTitle("Error");
+			alert.setHeaderText("I/O Error");
+			alert.setContentText("An error was encountered while saving the file: " + e.getMessage());
+			alert.showAndWait();
 		}
 	}
 }
